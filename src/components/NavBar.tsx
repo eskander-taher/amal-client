03d95cbfed00c1fd3330c51d75659169b260f9bc
eskander-taher <eskander.alshaibany@gmail.com>
2025-07-28 11:29:28 +0300

    load font

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -28,9 +28,9 @@ export default function NavBar() {
 	return (
 		<nav className="bg-white/90 backdrop-blur border-b border-gray-200 shadow-sm sticky top-0 z-50">
 			<div className="max-w-7xl mx-auto px-4 py-4">
-				
 				<div className="flex items-center justify-between">
 					{/* Logo */}
+
 					<Link href="/">
 						<div className="flex items-center space-x-2 space-x-reverse">
 							<Image
