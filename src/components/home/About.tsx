2ca16a1211ecbce86b5c382814529080d7842fcb
eskander-taher <eskander.alshaibany@gmail.com>
2025-07-30 03:57:43 +0300

    finish about section

--- src/components/home/About.tsx
+++ src/components/home/About.tsx
@@ -1,21 +1,30 @@
-import React from "react";
-import { useTranslations } from "next-intl";
+import Image from "next/image";
+import { useLocale, useTranslations } from "next-intl";
+import MaxWidthWrapper from "../MaxWidthWrapper";
 
 const About: React.FC = () => {
 	const t = useTranslations("HomePage");
+	const locale = useLocale();
+
 	return (
 		<section className="relative bg-white py-12 md:py-20 px-4 text-center">
-			<div className="max-w-2xl mx-auto flex">
-				<h2 className="text-2xl md:text-3xl font-bold mb-4 text-gray-800">
-					{t("aboutTitle")}
-				</h2>
-				<p className="text-gray-600 text-lg leading-relaxed">{t("aboutDescription")}</p>
-			</div>
+			<MaxWidthWrapper className="flex gap-20" maxWidth="6xl">
+				<Image
+					src={locale == "ar" ? "/about.svg" : "/amal_big_logo_en.png"}
+					alt="Amal Al-Khair logo"
+					width={260}
+					height={75}
+					priority
+				/>
+				<p className="text-gray-600 text-lg leading-relaxed text-right text-justify">{t("aboutDescription")}</p>
 
-			{/* Bottom static curve */}
-			<div className="absolute w-64 h-8 bottom-0 left-0 transform translate-x-1/2 translate-y-1/2 bg-gray-200 rounded-t-full flex justify-center items-top pt-2 gap-3">
-				<h3 className="absolute cursor-pointer -translate-y-[150%]">المزيد عن أمل الخير</h3>
-			</div>
+				{/* Bottom static curve */}
+				<div className="absolute w-64 h-8 bottom-0 left-0 transform translate-x-1/2 translate-y-1/2 bg-gray-200 rounded-t-full flex justify-center items-top pt-2 gap-3">
+					<h3 className="absolute cursor-pointer -translate-y-[150%]">
+						المزيد عن أمل الخير
+					</h3>
+				</div>
+			</MaxWidthWrapper>
 		</section>
 	);
 };
