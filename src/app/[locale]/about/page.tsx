50854758b5ee230294dcd406abb3b4f9fc1d8656
eskander-taher <eskander.alshaibany@gmail.com>
2025-08-07 22:11:33 +0300

    finish content of about page in arabic

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -2,23 +2,21 @@ import Hero from "@/components/Hero";
 import Aboutus from "@/components/about/Aboutus";
 import React from "react";
 import CompanyOverview from "@/components/about/CompanyOverview";
+import AboutImageSection from "@/components/about/AboutImageSection";
 import DateCompanySection from "@/components/about/DateCompanySection";
 import ContactSection from "@/components/about/ContactSection";
 import NewsletterSection from "@/components/home/Newsletter";
 
 export default function AboutPage() {
   return (
-    <div className="relative min-h-[60vh] flex flex-col items-center justify-center overflow-hidden">
-      <div className="absolute inset-0 z-0 bg-[url('/bg.jpg')] bg-repeat opacity-3" />
-
-      <div className="relative z-10 w-full">
-        <Hero title="حول امل الخير"/>
-        <Aboutus />
-        <CompanyOverview />
-        <DateCompanySection />
-        <ContactSection />
-        <NewsletterSection/>
-      </div>
-    </div>
+		<div className="relative flex flex-col gap-0">
+			<Hero title="حول امل الخير" />
+			<Aboutus />
+			<AboutImageSection />
+			<CompanyOverview />
+			<DateCompanySection />
+			<ContactSection />
+			<NewsletterSection />
+		</div>
   );
 }
---
+++ src/components/about/AboutImageSection.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+export default function AboutImageSection() {
+	return (
+		<section className="py-16 bg-white">
+			<div className="w-full">
+				<Image
+					src="/about.jpg"
+					alt="About Amal Al-Khair"
+					width={1920}
+					height={800}
+					className="w-full h-auto"
+					priority
+				/>
+			</div>
+		</section>
+	);
+}
